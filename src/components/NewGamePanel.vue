3294a99513a8a7d7c3316dfee9bee8d090d05474
Bram Bekkers <bram.bekkers@outlook.com>
2021-04-09 10:29:13 +0200

    new game

--- src/components/NewGamePanel.vue
+++ src/components/NewGamePanel.vue
@@ -2,19 +2,13 @@
 	<div id="panel">
 		<h1>OR Jaar(verslag)spel</h1>
 		<p>
-			Welkom bij het OR-Jaarverslag spel. Met dit spel willen we jullie op
-			een leuke manier mee terug nemen naar het or-jaar 2020. Het was een
-			roerig jaar door Corona waar we het allemaal zwaar mee hebben gehad.
-			Tijdens dit spel kom je achter alle OR-onderwerpen die dit jaar
-			hebben gespeeld.
+			Welkom bij het OR-Jaarverslag spel. Met dit spel willen we jullie op een
+			leuke manier mee terug nemen naar het or-jaar 2020. Het was een roerig jaar
+			door Corona waar we het allemaal zwaar mee hebben gehad. Tijdens dit spel kom
+			je achter alle OR-onderwerpen die dit jaar hebben gespeeld.
 		</p>
 		<p class="mb-1">Wat is jouw naam:</p>
-		<input
-			id="name"
-			class="animate__animated"
-			v-model="name"
-			type="text"
-		/><br />
+		<input id="name" class="animate__animated" v-model="name" type="text" /><br />
 
 		<p>Speel het spel en probeer 2020 te verslaan.</p>
 		<a class="btn" type="submit" @click="click()">Start het spel</a>
@@ -22,40 +16,39 @@
 </template>
 
 <script>
-	const clickSound = new Audio(require("@/assets/sounds/click.ogg"));
+const clickSound = new Audio(require("@/assets/sounds/click.ogg"));
 
-	export default {
-		computed: {
-			name: {
-				// getters
-				get() {
-					return this.$store.getters.name;
-				},
-				// setter
-				set(val) {
-					this.$store.commit("name", val);
-				},
+export default {
+	computed: {
+		name: {
+			// getters
+			get() {
+				return this.$store.getters.name;
+			},
+			// setter
+			set(val) {
+				this.$store.commit("name", val);
 			},
 		},
-		methods: {
-			click() {
-				const el = document.getElementById("name");
+	},
+	methods: {
+		click() {
+			const el = document.getElementById("name");
 
-				if (this.name) {
-					clickSound.play();
-					this.$store.commit("gameStarted", true);
-				} else {
-					el.style.background = "#CD6C43";
-					el.classList.add("animate__shakeX");
-				}
-				setTimeout(() => {
-					el.classList.remove("animate__shakeX");
-					el.style.background = "#FFFFFF";
-				}, 1000);
-			},
+			if (this.name) {
+				clickSound.play();
+				this.$store.commit("gameStarted", true);
+			} else {
+				el.style.background = "#CD6C43";
+				el.classList.add("animate__shakeX");
+			}
+			setTimeout(() => {
+				el.classList.remove("animate__shakeX");
+				el.style.background = "#FFFFFF";
+			}, 1000);
 		},
-	};
+	},
+};
 </script>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
